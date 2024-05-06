937398005b303029dacd51d726a2cfbb1acd55ab
vu3.nguyen <vu3.nguyen@lge.com>
2024-05-06 19:57:30 +0900

    update

---
+++ GNU_Autotools/SampleProjects/01_MultipleSourceFiles/simple_math.h
@@ -0,0 +1,17 @@
+#ifndef SIMPLE_MATH_H
+#define SIMPLE_MATH_H
+
+namespace simple_math
+{
+    int add(int a, int b);
+    int subtract(int a, int b);
+    int multiply(int a, int b);
+    double divide(int a, int b);
+    int power(int base, int exponent);
+    double square_root(double value);
+    double sine(double angle);
+    double cosine(double angle);
+    double tangent(double angle);
+}
+
+#endif
